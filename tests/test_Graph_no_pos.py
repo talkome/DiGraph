5d79e4c6b6b066c7f10e69448bc372a66a814978
talcome <talkome@gmail.com>
2021-01-12 18:14:20 +0200

    add graphs and tests

---
+++ tests/test_Graph_no_pos.py
@@ -0,0 +1,52 @@
+from src.GraphAlgo import GraphAlgo
+
+
+def create_g_10_80_0():
+    graph = GraphAlgo()
+    file = "../data/Graph_no_pos/G_10_80_0.json"
+    graph.load_from_json(file)
+    graph.plot_graph()
+
+
+def create_g_100_800_0():
+    graph = GraphAlgo()
+    file = "../data/Graph_no_pos/G_100_800_0.json"
+    graph.load_from_json(file)
+    graph.plot_graph()
+
+
+def create_g_1000_8000_0():
+    graph = GraphAlgo()
+    file = "../data/Graph_no_pos/G_1000_8000_0.json"
+    graph.load_from_json(file)
+    graph.plot_graph()
+
+
+def create_g_10000_80000_0():
+    graph = GraphAlgo()
+    file = "../data/Graph_no_pos/G_10000_80000_0.json"
+    graph.load_from_json(file)
+    graph.plot_graph()
+
+
+def create_g_20000_160000_0():
+    graph = GraphAlgo()
+    file = "../data/Graph_no_pos/G_20000_160000_0.json"
+    graph.load_from_json(file)
+    graph.plot_graph()
+
+
+def create_g_30000_240000_0():
+    graph = GraphAlgo()
+    file = "../data/Graph_no_pos/G_30000_240000_0.json"
+    graph.load_from_json(file)
+    graph.plot_graph()
+
+
+if __name__ == '__main__':
+    create_g_10_80_0()
+    create_g_100_800_0()
+    create_g_1000_8000_0()
+    create_g_10000_80000_0()
+    create_g_20000_160000_0()
+    create_g_30000_240000_0()
---
+++ tests/test_Graph_on_circle.py
@@ -0,0 +1,46 @@
+from src.GraphAlgo import GraphAlgo
+
+
+def create_g_10_80_1():
+    graph = GraphAlgo()
+    graph.load_from_json("../data/Graph_on_circle/G_10_80_1.json")
+    graph.plot_graph()
+
+
+def create_g_100_800_1():
+    graph = GraphAlgo()
+    graph.load_from_json("../data/Graph_on_circle/G_100_800_1.json")
+    graph.plot_graph()
+
+
+def create_g_1000_8000_1():
+    graph = GraphAlgo()
+    graph.load_from_json("../data/Graph_on_circle/G_1000_8000_1.json")
+    graph.plot_graph()
+
+
+def create_g_10000_80000_1():
+    graph = GraphAlgo()
+    graph.load_from_json("../data/Graph_on_circle/G_10000_80000_1.json")
+    graph.plot_graph()
+
+
+def create_g_20000_160000_1():
+    graph = GraphAlgo()
+    graph.load_from_json("../data/Graph_on_circle/G_20000_160000_1.json")
+    graph.plot_graph()
+
+
+def create_g_30000_240000_1():
+    graph = GraphAlgo()
+    graph.load_from_json("../data/Graph_on_circle/G_30000_240000_1.json")
+    graph.plot_graph()
+
+
+if __name__ == '__main__':
+    create_g_10_80_1()
+    create_g_100_800_1()
+    create_g_1000_8000_1()
+    create_g_10000_80000_1()
+    create_g_20000_160000_1()
+    create_g_30000_240000_1()
